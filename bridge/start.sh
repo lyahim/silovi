75bd95a4c24fa27606c5dd78a61cfec2c39004db
lyahim <lyahim@gmail.com>
2021-03-04 14:18:54 +0100

    fix slashes in bridge start.sh

--- bridge/start.sh
+++ bridge/start.sh
@@ -6,4 +6,4 @@
 # set JAVA_HOME if needed
 # export JAVA_HOME=
 
-java -jar .\bin\silovi-bridge.jar --spring.config.location=file:config.properties > /dev/null 2>&1 &
+java -jar ./bin/silovi-bridge.jar --spring.config.location=file:config.properties > /dev/null 2>&1 &
